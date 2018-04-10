dcd06b1cfbdc87de578c16d5387f6193e4b6ba8f
Unknown <anthonydeniaud@gmail.com>
2018-04-10 21:20:43 +0200

    svg logo animated

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 console.log('This would be the main JS file.');
+
+$(function(){
+  var $svg = $(".svg-container");
+  var phi = 1.618;
+  var appWidth = window.innerWidth;
+  var appHeight = window.innerHeight;
+  console.log("svg",appWidth,"---",appHeight);
+  var svgContainerWidth = appWidth/phi;
+  var svgContainerHeight = appHeight/phi;
+  console.log("svg",svgContainerWidth,"---",svgContainerHeight);
+
+  $svg.css('width',svgContainerWidth);
+  $svg.css('height',svgContainerHeight);
+
+  $svg.addClass('animating');
+  console.log($svg.length);
+
+});
