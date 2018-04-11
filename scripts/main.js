c42fb7a69dd709d07a857cba1e4a53e5de448a2e
Unknown <anthonydeniaud@gmail.com>
2018-04-11 18:47:46 +0200

    wip svg logo animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,21 @@
+var phi = 1.618;
 console.log('This would be the main JS file.');
 
 $(function(){
-  var $svg = $(".svg-container");
-  var phi = 1.618;
+  var $logo = $(".logo");
   var appWidth = window.innerWidth;
   var appHeight = window.innerHeight;
   console.log("svg",appWidth,"---",appHeight);
+/*
   var svgContainerWidth = appWidth/phi;
   var svgContainerHeight = appHeight/phi;
+
   console.log("svg",svgContainerWidth,"---",svgContainerHeight);
 
-  $svg.css('width',svgContainerWidth);
-  $svg.css('height',svgContainerHeight);
-
-  $svg.addClass('animating');
-  console.log($svg.length);
+  $logo.css('width',svgContainerWidth);
+  $logo.css('height',svgContainerHeight);
+*/
+  $logo.addClass('logo--animating');
+  console.log($logo.length);
 
 });
