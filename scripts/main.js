4a2cafc395bf1dd052a98defe4429ddf8fc10997
Unknown <anthonydeniaud@gmail.com>
2018-06-09 17:27:38 +0200

    Nice new Start

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,94 @@
 var phi = 1.618;
 console.log('This would be the main JS file.');
 
-var widthMAx = screen.width;
+var debug=true;
+
+var widthMax = screen.width;
 var heightMax = screen.height;
 
 var width = window.innerWidth;
 var height = window.innerHeight;
 
+var doit;
+
+/*
 var size = 32;
 var nbTileX = Math.round(width/size);
 var nbTileY = Math.round(height/size);
+*/
+//console.log("w/h:",nbTileX,",",nbTileY);
+function parralax(event){
+	/*
+	clearTimeout(doit);
+	doit = setTimeout(function(){
+	*/
+	//console.log(event);
+	var mouseTop = event.clientY;
+	var mouseLeft = event.clientX;
+	var verticaleOffset = heightMax/2-mouseTop;
+	var horizontaleOffset = widthMax/2-mouseLeft;
+	//console.log(heightMax/widthMax,widthMax/heightMax);
+	var amplitudeH = 0.001;
+	var amplitudeV = 0.004;
 
-console.log("w/h:",nbTileX,",",nbTileY);
+	var $etherLayer = $(".area-ether__layer");
+	$etherLayer.each(function(i,el){
+		var index=i;
+		var verticale = verticaleOffset*index*amplitudeV;
+		var horizontale = horizontaleOffset*index*amplitudeH;
+		$(el).css('transform','translate('+horizontale+'%,'+verticale+'%)');
+		//console.log(i,el);
+	});
+
+	//}, 16);
+}
+
+function logoActive(){
+	var $logo = $('.logo');
+	var $bg = $('.area-background');
+	$logo.toggleClass('logo--active');
+	$bg.toggleClass('area-background--active');
+}
 
 $(function(){
-	var $tiletmpl = $("#tiletmpl");
+	setTimeout(function(){
+		$('body').addClass('app--ready');
+	},1000);
+	//console.log("loaded");
+	$("body").on('mousemove',parralax);
+	$(".logo").on('click',logoActive);
+
+
+
+
+	//	var $tiletmpl = $("#tiletmpl");
 	/*
 	$tile.width();
 	$tile.height();
 	*/
-
+	/*
 	var $tileContainer = $("#tile-container");
 	for(var i =0; i <nbTileX*nbTileY ;i++){
-		$tileContainer.append($tiletmpl.html());
-	}
+	$tileContainer.append($tiletmpl.html());
+}*/
 
-	/*
-	var $app = $(".app");
-	var $logo = $(".logo");
-	var appWidth = window.innerWidth;
-	var appHeight = window.innerHeight;
-	console.log("svg",appWidth,"---",appHeight);
-	setTimeout(function(){
-		console.log("grow");
-		//$logo.find(".logo__svg").addClass('logo__svg--ready');
-		//$logo.find(".logo__svg").addClass('logo__svg--ready');
-	},2700);
-	console.log($logo.length);
+/*
+var $app = $(".app");
+var $logo = $(".logo");
+var appWidth = window.innerWidth;
+var appHeight = window.innerHeight;
+console.log("svg",appWidth,"---",appHeight);
+setTimeout(function(){
+console.log("grow");
+//$logo.find(".logo__svg").addClass('logo__svg--ready');
+//$logo.find(".logo__svg").addClass('logo__svg--ready');
+},2700);
+console.log($logo.length);
 
 function lighting(){
-	console.log(">Lighting Scene");
-	//$app.addClass('app--lighting');
-	$logo.addClass('logo--lighting');
+console.log(">Lighting Scene");
+//$app.addClass('app--lighting');
+$logo.addClass('logo--lighting');
 }
 
 $logo.find('.logo__letter--x').one('animationend',lighting);
@@ -51,14 +98,14 @@ $logo.find('.logo__letter--x').one('animationend',lighting);
 $('.card__trigger').on('click touch',function(){$(this).closest('.card').toggleClass('card--open')});
 
 setTimeout(function(){
-	console.log("app_ready");
-	$app.addClass('app--ready');
-	$logo.addClass('logo--animating');
+console.log("app_ready");
+$app.addClass('app--ready');
+$logo.addClass('logo--animating');
 },2500);
 
 setTimeout(function(){
-	console.log("app_ready");
-	$logo.addClass('logo--growth');
+console.log("app_ready");
+$logo.addClass('logo--growth');
 },8500);
 
 */
